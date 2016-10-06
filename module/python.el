610036e3f6064819fc1563639eedb184cd980285
Markus Faerevaag <m@frv.ag>
2016-10-06 12:10:58 +0200

    add python file hook

--- module/python.el
+++ module/python.el
@@ -1,6 +1,7 @@
 (use-package python
   :if personal-init-full
   :ensure t
+  :mode "\\.py\\'"
   :config
   (define-key python-mode-map (kbd "C-c C-c")
   (lambda () (interactive) (python-shell-send-buffer t)))
