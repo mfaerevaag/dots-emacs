81fc6955e8cb81e9446b60deb8932a26c27e278d
Markus Faerevaag <m@frv.ag>
2016-10-14 17:50:07 +0200

    some irc

--- module/irc.el
+++ module/irc.el
@@ -7,51 +7,45 @@
 
   :config
 
-  (setq-default rcirc-server-alist '(("irc.freenode.net"
-                                      :channels ()
-                                      :nick "slevin"
-                                      :encryption tls
-                                      :port 6697)
-                                     ("localhost"
-                                      :nick "markus"
-                                      :port 6667
-                                      :channels ("&facebook"))))
-  (setq-default rcirc-authinfo '(("freenode" nickserv "slevin" "GdFuudT7FefG4XK2Gn1RnjamyoS0L3D83RaROic1k10ajy5zxr")
-                                 ("localhost" bitlbee "markus" "B2i2L.kjas.")))
 
-  (add-hook 'rcirc-mode-hook
-            (lambda ()
-              ;; keep input line at bottom.
-              (set (make-local-variable 'scroll-conservatively) 8192)
-              ;; spellcheck
-              (flyspell-mode 1)
-              (ispell-change-dictionary "norsk")
-              ;; window config
-              (winner-mode 1)))
+  (add-hook 'rcirc-mode-hook #'(flyspell-mode
+                                rcirc-track-minor-mode
+                                winner-mode))
+
+  ;; (ispell-change-dictionary "norsk")
 
   ;; formating
-  (setq rcirc-prompt      "»» "
-        rcirc-time-format "%H:%M "
-        ;; autofill off
-        rcirc-fill-flag   nil)
+  (setq rcirc-prompt "»» "
+        rcirc-fill-column 80
+        ;; rcirc-fill-flag nil
+        rcirc-buffer-maximum-lines 2048
+        rcirc-omit-responses '("JOIN" "PART" "QUIT" "NICK" "AWAY" "MODE")
+        rcirc-time-format "%Y-%m-%d %H:%M "
+        rcirc-omit-threshold 20)
+
+  (set (make-local-variable 'scroll-conservatively) 8192)
 
   ;; extensions
   (require 'rcirc-groups)
-  ;; (setq rcirc-groups:display-all t)
-  (defun rcirc-groups:switch-to-conversation-buffer ()
-    "switch from *rcirc-groups* buffer to referenced one"
-    (interactive)
-    (let ((conversation-buffer
-           (buffer-substring (line-beginning-position) (line-end-position))))
-      (rcirc-groups:catchup-conversation)
-      (set-window-buffer (other-window 1) conversation-buffer)))
 
   (require 'rcirc-alertify)
   (rcirc-alertify-enable)
   (setq alert-default-style 'libnotify)
 
+  (setq rcirc-server-alist '(
+                             ("bitlbee"
+                              :host "138.68.80.199"
+                              :port 6668
+                              :user-name "markus"
+                              :auth "markus/bitlbee"
+                              :encryption tls)
+                             ))
+
   (add-hook 'after-init-hook (lambda ()
-                               (rcirc-connect "irc.freenode.net")
-                               (rcirc-connect "localhost")
+                               (setq rcirc-server-alist
+                                     ;; This will replace :auth with the correct thing,
+                                     ;; see the doc for that function
+                                     (personal/znc-rcirc-server-alist-get-authinfo rcirc-server-alist))
+                               (personal/znc-rcirc-connect)
                                ))
   )
--- package/yasnippet.el
+++ package/yasnippet.el
@@ -1,7 +1,8 @@
 (use-package yasnippet
   :diminish yas-minor-mode
   :config
-  (add-hook 'fundamental-mode-hook #'yas-minor-mode)
-  (add-hook 'prog-mode-hook #'yas-minor-mode)
   (add-to-list 'yas-snippet-dirs (concat personal-dir "snippets"))
-  (yas-reload-all))
+  (yas-reload-all)
+
+  (add-hook 'fundamental-mode-hook #'yas-minor-mode)
+  (add-hook 'prog-mode-hook #'yas-minor-mode))
